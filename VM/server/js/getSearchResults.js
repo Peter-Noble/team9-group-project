cc7cc9ad974c7ae06ac9cdea0a254c6cd8438451
Thomas Marsh <marsh.thomaslewis@gmail.com>
2017-03-16 10:55:53 +0000

    Show search results in item list form

---
+++ VM/server/js/getSearchResults.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+
+    // had to copy this in from formatDateScript.pug as I wasn't sure how else to make prettyDate available
+    // for use in this script
+    function prettyDate(dateString){
+      //if it's already a date object and not a string you don't need this line:
+        var date = new Date(dateString);
+        var d = date.getDate();
+        var monthNames = [ "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" ];
+        var m = monthNames[date.getMonth()];
+        var y = date.getFullYear();
+        return d+' '+m+' '+y;
+    }
+
+    // Function for retrieving get parameters from url
+    var getUrlParameter = function getUrlParameter(sParam) {
+        var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+          sURLVariables = sPageURL.split('&'),
+          sParameterName,
+          i;
+
+        for (i = 0; i < sURLVariables.length; i++) {
+            sParameterName = sURLVariables[i].split('=');
+
+            if (sParameterName[0] === sParam) {
+                  return sParameterName[1] === undefined ? true : sParameterName[1];
+            }
+        }
+    };
+    var text = getUrlParameter('searchtext');
+    $.ajax({
+        url: "/search",
+        data: {searchtext: text},
+        success: function(data) {
+            $.each(JSON.parse(data), function(index, item) {
+                $('#results').append("<li><a href='/item/" + item.Listing_ID + "'><h3>" + item.Title + "</h3></a>" +
+                "<p>" + prettyDate(item.Expiry) + "</p><p>Image goes here</p>");
+            })
+        }
+    })
+})
