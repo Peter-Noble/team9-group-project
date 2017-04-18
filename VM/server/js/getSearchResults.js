32ccb1988348b9b01916702cd1bfa4a5aef82996
Nathan Giles <nathanjgiles@gmail.com>
2017-04-18 18:34:49 +0100

    URL changes based on order criteria

--- VM/server/js/getSearchResults.js
+++ VM/server/js/getSearchResults.js
@@ -13,58 +13,18 @@ $(document).ready(function() {
         var y = date.getFullYear();
         return d+' '+m+' '+y;
     }
-
-    // Function for retrieving get parameters from url
-    var getUrlParameter = function getUrlParameter(sParam) {
-        var sPageURL = decodeURIComponent(window.location.search.substring(1)),
-          sURLVariables = sPageURL.split('&'),
-          sParameterName,
-          i;
-
-        for (i = 0; i < sURLVariables.length; i++) {
-            sParameterName = sURLVariables[i].split('=');
-
-            if (sParameterName[0] === sParam) {
-                  return sParameterName[1] === undefined ? true : sParameterName[1];
-            }
-        }
-    };
-    var text = getUrlParameter('searchtext');
-    var sortParam = getUrlParameter('sortBy');
-    console.log(sortParam);
-    $.ajax({
+    
+    // Render results
+    function renderListings(){
+        $.ajax({
         url: "/search",
         data: {searchtext: text, sort: sortParam},
         success: function(data) {
             var parsedData = JSON.parse(data);
             
-            // Sorting function (date added)
-            var sorted = []
-            console.log(Object.keys(parsedData).length);
-            for (obj in parsedData){
-                sorted.push(parsedData[obj]);
-            }
-            
-            switch(sortParam) {
-                case "dateASC":
-                    console.log("dateASC used to sort");
-                    sorted.sort(function(a, b){
-                        return new Date(a.Added).getTime() - new Date(b.Added).getTime()});
-                    break;
-                case "dateDSC":
-                    console.log("dateDSC used to sort");
-                    sorted.sort(function(a, b){
-                        return new Date(b.Added).getTime() - new Date(a.Added).getTime()});
-                    break;
-                default:
-                    console.log("default used to sort");
-                    sorted.sort(function(a, b){
-                        return new Date(a.Added).getTime() - new Date(b.Added).getTime()});
-                    break;
-            }
-            
-            console.log(sorted);
-            // End sorting function
+            // Sorting results
+            var sorted = sortBy(parsedData, sortParam);
+            // End sorting
             
             $.each(sorted, function(index, item) {
                 var image = "";
@@ -91,6 +51,51 @@ $(document).ready(function() {
             updateMapSearchResults(mapItems);
         }
     })
+    }
+    
+    // sorting function
+    function sortBy(parsed, param){
+        sorting = []
+        for (obj in parsed){
+                sorting.push(parsed[obj]);
+            }
+        switch(param) {
+            case "dateASC":
+                sorting.sort(function(a, b){
+                    return new Date(a.Added).getTime() - new Date(b.Added).getTime()});
+                break;
+            case "dateDSC":
+                sorting.sort(function(a, b){
+                    return new Date(b.Added).getTime() - new Date(a.Added).getTime()});
+                break;
+            default:
+                sorting.sort(function(a, b){
+                    return new Date(a.Added).getTime() - new Date(b.Added).getTime()});
+                break;
+        }
+        return sorting;
+    }
+    
+    // Function for retrieving get parameters from url
+    var getUrlParameter = function getUrlParameter(sParam) {
+        var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+          sURLVariables = sPageURL.split('&'),
+          sParameterName,
+          i;
+
+        for (i = 0; i < sURLVariables.length; i++) {
+            sParameterName = sURLVariables[i].split('=');
+
+            if (sParameterName[0] === sParam) {
+                  return sParameterName[1] === undefined ? true : sParameterName[1];
+            }
+        }
+    };
+    var text = getUrlParameter('searchtext');
+    var sortParam = getUrlParameter('sortBy');
+    
+    renderListings();
+
     $("#postcode").on("input", function(e) {
         clearTimeout(searchRadiusPostcodeTimeout);
         searchRadiusPostcodeTimeout = setTimeout(function() {
@@ -114,3 +119,11 @@ $(document).ready(function() {
         searchRadiusCircle.setRadius(parseInt($("#searchRadius")[0].value) * 1000);
     })
 })
+
+function refineSearch(sortParam){
+    var url = document.location.toString();
+    var relativeURL = url.split("/").pop();
+    var smallURL = relativeURL.substring(0, relativeURL.indexOf("sortBy"));
+    var newURL = smallURL.concat("sortBy=", sortParam);
+    history.pushState(null, null, newURL);
+}
